f8c37de26cabb6262c40a103c6973323a357438e
fuchdgdudhdhdhdh-ai <fuchdgdudhdhdhdh@gmail.com>
2026-08-29 11:25:38 +0500

    Update content.py

--- content.py
+++ content.py
@@ -14,7 +14,7 @@ import random
 
 CINEMA_NAME = "Супер 8"
 CINEMA_CITY = "Йошкар-Ола"
-SHOWTIME_DATE = "28 августа 2026 года"
+SHOWTIME_DATE = "29 августа 2026 года"
 
 WELCOME_TEXT = (
     f"🎬 <b>Добро пожаловать в кинотеатр «{CINEMA_NAME}»!</b>\n"
